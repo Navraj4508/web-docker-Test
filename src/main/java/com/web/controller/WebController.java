0927f3b9735e81d65008372b5de388c8a63b34a0
Navrajk4508@gmail.com <navrajk4508@gmail.com>
2025-09-28 01:43:16 +0530

    docker test

---
+++ src/main/java/com/web/controller/WebController.java
@@ -0,0 +1,14 @@
+package com.web.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebController {
+
+	@RequestMapping("/test")
+	public String test() {
+		return "Hello World";
+	}
+	
+}
